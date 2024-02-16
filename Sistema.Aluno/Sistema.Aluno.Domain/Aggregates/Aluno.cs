7f92680b0709ddfe76c2a4288041d4fe5cff2df3
Renato Hack <renatohack@outlook.com>
2024-02-16 09:20:18 -0300

    metodos de classe Aluno definidos

--- Sistema.Aluno/Sistema.Aluno.Domain/Aggregates/Aluno.cs
+++ Sistema.Aluno/Sistema.Aluno.Domain/Aggregates/Aluno.cs
@@ -9,10 +9,75 @@ namespace Sistema.Aluno.Domain.Aggregates
     public class Aluno
     {
 
-        public Guid Id { get; set; }
+        // properties
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string Nome { get; set; }
         public string Matricula { get; set; }
-        public List<Inscricao> Inscricoes { get; set; }
+        public List<Inscricao> Inscricoes { get; set; } = new List<Inscricao>();
+
+       
+
+        // public methods
+        public static Aluno CriarAluno(string nome, string matricula)
+        {
+            Aluno aluno = new Aluno()
+            {
+                Nome = nome,
+                Matricula = matricula
+            };
+
+            return aluno;
+        }
+
+        public void RealizarInscricao(Turma turma)
+        {
+            Inscricao inscricao = new Inscricao()
+            {
+                Turma = turma,
+                Aluno = this,
+            };
+
+            this.Inscricoes.Add(inscricao);
+            turma.Inscricoes.Add(inscricao);
+        }
+
+        public void CancelarInscricao(Guid idInscricao)
+        {
+            this.Inscricoes.FirstOrDefault(inscricao => inscricao.Id == idInscricao).CancelarInscricao();
+        }
+
+        public List<float> ConsultarNotasDeDisciplina(Guid idInscricao)
+        {
+            return this.Inscricoes.FirstOrDefault(inscricao => inscricao.Id == idInscricao).ObterNotas();
+        }
+
+        public float ConsultarFrequenciaEmDisciplina(Guid idInscricao)
+        {
+            return this.Inscricoes.FirstOrDefault(inscricao => inscricao.Id == idInscricao).ObterFrequencia();
+        }
+
+        public List<Inscricao> ConsultarHistorico()
+        {
+            return this.Inscricoes;
+        }
+
+        public void AlterarNotasDeDisciplina(Guid idInscricao, float notap1, float notap2, float notapf)
+        {
+            Inscricao inscricao = this.Inscricoes.FirstOrDefault(inscricao => inscricao.Id == idInscricao);
+
+            inscricao.NotaP1 = notap1;
+            inscricao.NotaP2 = notap2;
+            inscricao.NotaPF = notapf;
+        }
+
+        public void AlterarFrequenciaEmDisciplina(Guid idInscricao, int presenca)
+        {
+            Inscricao inscricao = this.Inscricoes.FirstOrDefault(inscricao => inscricao.Id == idInscricao);
+
+            inscricao.Presenca = presenca;
+        }
+
+
 
     }
 }
--- Sistema.Aluno/Sistema.Aluno.Domain/Aggregates/Inscricao.cs
+++ Sistema.Aluno/Sistema.Aluno.Domain/Aggregates/Inscricao.cs
@@ -8,14 +8,35 @@ namespace Sistema.Aluno.Domain.Aggregates
 {
     public class Inscricao
     {
-        public Guid Id { get; set; }
-        public bool Ativa { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public bool Ativa { get; set; } = true;
         public Turma Turma { get; set; }
         public Aluno Aluno { get; set; }
-        public float Frequencia { get; set; }
-        public float NotaP1 { get; set; }
-        public float NotaP2 { get; set; }
-        public float NotaPF { get; set; }
+        private float Frequencia { get; set; } = 0;
+        public int Presenca { get; set; } = 0;
+        public float NotaP1 { get; set; } = 0;
+        public float NotaP2 { get; set; } = 0;
+        public float NotaPF { get; set; } = 0;
+
+
+
+        // public methods
+        public void CancelarInscricao()
+        {
+            this.Ativa = false;
+        }
+
+        public List<float> ObterNotas()
+        {
+            return new List<float>() { this.NotaP1, this.NotaP2, this.NotaPF };
+        }
+
+        public float ObterFrequencia()
+        {
+            return this.Presenca / this.Turma.NumeroAulas;
+        }
+
+
 
     }
 }
