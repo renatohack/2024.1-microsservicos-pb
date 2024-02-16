f513895e261ca5e800026d0a2ddfea1bce1c421e
Renato Hack <renatohack@outlook.com>
2024-02-16 09:59:36 -0300

    inicializados testes de classe aluno

---
+++ Sistema.Aluno/Sistema.Aluno.Test/Domain/AlunoTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain = Sistema.Aluno.Domain.Aggregates;
+
+namespace Sistema.Aluno.Test.Domain
+{
+    public class AlunoTest
+    {
+
+        [Fact]
+        public void DeveCriarAlunoComSucesso()
+        {
+            domain.Aluno aluno = new domain.Aluno()
+            {
+                Nome = "Jorge",
+                Matricula = "1"
+            };
+
+
+            Assert.NotNull(aluno);
+            Assert.True(aluno.Id != Guid.Empty);
+            Assert.False(String.IsNullOrEmpty(aluno.Nome));
+            Assert.False(String.IsNullOrEmpty(aluno.Matricula));
+            Assert.NotNull(aluno.Inscricoes);
+            Assert.False(aluno.Inscricoes.Any());
+
+        }
+
+    }
+}
