c79ff37e4d4211ba770fceb9209d6f9e78878aee
Bruno Verdan <bruno.verdan@al.infnet.edu.br>
2024-03-21 13:29:23 -0300

    Criação do método de criar aluno no projeto Secretaria

---
+++ Sistema.Secretaria/Sistema.Secretaria.API/Controllers/SecretariaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema.Secretaria.Service.Secretaria;
+
+namespace Sistema.Secretaria.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SecretariaController : ControllerBase
+{
+    public SecretariaService Service { get; set; }
+
+    public SecretariaController()
+    {
+        Service = new SecretariaService();
+    }
+
+    [HttpPost("criarAluno")]
+    public IActionResult CriarAluno(AlunoReqeust reqeust)
+    {
+        Service.CriarAluno(reqeust.Nome);
+
+        return Ok();
+    }
+}
+
+public class AlunoReqeust
+{
+    public string Nome { get; set; }
+}
---
+++ Sistema.Secretaria/Sistema.Secretaria.Service/Secretaria/SecretariaService.cs
@@ -0,0 +1,17 @@
+﻿using Sistema.Secretaria.Repository.Secretaria;
+
+namespace Sistema.Secretaria.Service.Secretaria;
+public class SecretariaService
+{
+    public SecretariaRepository Repository { get; set; }
+
+    public SecretariaService()
+    {
+        Repository = new SecretariaRepository();
+    }
+
+    public void CriarAluno(string nome)
+    {
+        Repository.CriarAluno(nome);
+    }
+}
